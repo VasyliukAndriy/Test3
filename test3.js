d685bf8e50f797c9ad8cbbbe296c8b5c02d4a5c9
VasyliukAndriy <144329579+VasyliukAndriy@users.noreply.github.com>
2023-10-30 21:10:46 +0200

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,339 +1,66 @@
-let anythingnumber;
-let firstword = '';
-let secondword = '';
-let thirdword = '';
-let sizearray = '';
+//---Поле для вводу числа
+const num = 10;
+//---Поле для вводу числа
+let str = num.toString();
 
-anythingnumber = prompt();
+let str1 = '';
+let str2 = '';
+let result = '';
 
-arrayofStrings = Array.from(String(anythingnumber));
-
-if (anythingnumber.length === 1) {
-    from0to9();
-} else if (anythingnumber.length === 2) {
-    let numberToCompare = arrayofStrings[1];
-    if (numberToCompare === '1')
-        from10to19()
-    else
-        from20to99()
-} else if (anythingnumber.length === 3) {
-    let numberToCompare = arrayofStrings[1];
-    if (numberToCompare === '1')
-        from100to999with10to19()
-
-    else
-        from100to999without10to19()
-
-} else {
-    console.log('Уведіть число від 0 до 999')
+if (num > 19 && num <= 99){
+    switch (str[0]){
+        case '2': str1 = 'Twenty '; break;
+        case '3': str1 = 'Thirty '; break;
+        case '4': str1 = 'Fourty '; break;
+        case '5': str1 = 'Fifty '; break;
+        case '6': str1 = 'Sixty '; break;
+        case '7': str1 = 'Seventy '; break;
+        case '8': str1 = 'Eighty '; break;
+        case '9': str1 = 'Ninty '; break;
+    }
+    switch (str[1]){
+        case '1': str2 = 'one'; break;
+        case '2': str2 = 'two'; break;
+        case '3': str2 = 'three'; break;
+        case '4': str2 = 'four'; break;
+        case '5': str2 = 'five'; break;
+        case '6': str2 = 'six'; break;
+        case '7': str2 = 'seven'; break;
+        case '8': str2 = 'eight'; break;
+        case '9': str2 = 'nine'; break;
+    }
+}   else if (num === 100){
+        str1 = 'One ';
+        str2 = 'hundred';
+}   else if (num >= 0 && num <= 9) {
+        switch (str[0]){
+            case '1': str1 = 'One'; break;
+            case '2': str1 = 'Two'; break;
+            case '3': str1 = 'Three'; break;
+            case '4': str1 = 'Four'; break;
+            case '5': str1 = 'Five'; break;
+            case '6': str1 = 'Six'; break;
+            case '7': str1 = 'Seven'; break;
+            case '8': str1 = 'Eight'; break;
+            case '9': str1 = 'Nine'; break;
+    }  
+}   else if (num >= 10 && num <= 19) {
+        switch (str){
+            case '10': str1 = 'Ten'; break;
+            case '11': str1 = 'Eleven'; break;
+            case '12': str1 = 'Tvelve'; break;
+            case '13': str1 = 'Thirteen'; break;
+            case '14': str1 = 'Fourteen'; break;
+            case '15': str1 = 'Fifteen'; break;
+            case '16': str1 = 'Sixteen'; break;
+            case '17': str1 = 'Seventeen'; break;
+            case '18': str1 = 'Eighteen'; break;
+            case '19': str1 = 'Ninteen'; break;
+        }
+}          
+    else {
+        str1 = 'Please enter the number from 1 to 100';
 }
 
-console.log(firstword + " " + secondword + " " + thirdword);
-
-
-function from0to9() {
-    switch (arrayofStrings[0]) {
-
-        case '0':
-            firstword = 'Нуль'
-            break;
-        case '1':
-            firstword = 'Один'
-            break;
-        case '2':
-            firstword = 'Два'
-            break;
-        case '3':
-            firstword = 'Три'
-            break;
-        case '4':
-            firstword = 'Чотири'
-            break;
-        case '5':
-            firstword = 'П\'ять'
-            break;
-        case '6':
-            firstword = 'Шість'
-            break;
-        case '7':
-            firstword = 'Сім'
-            break;
-        case '8':
-            firstword = 'Вісім'
-            break;
-        case '9':
-            firstword = 'Дев\'ять'
-            break;
-    }
-}
-
-
-function from10to99() {
-    if (arrayofStrings[0] === 1) {
-        from10to19();
-    } else
-
-        from20to99()
-
-}
-
-
-
-function from10to19() {
-    switch (arrayofStrings[1]) {
-        case '0':
-            firstword = 'Десять'
-            break;
-        case '1':
-            firstword = 'Одинадцять'
-            break;
-        case '2':
-            firstword = 'Дванадцять'
-            break;
-        case '3':
-            firstword = 'Тринадцять'
-            break;
-        case '4':
-            firstword = 'Чотирнадцять'
-            break;
-        case '5':
-            firstword = 'П\'ятнадцять'
-            break;
-        case '6':
-            firstword = 'Шістнадцять'
-            break;
-        case '7':
-            firstword = 'Сімнадцять'
-            break;
-        case '8':
-            firstword = 'Вісімнадцять'
-            break;
-        case '9':
-            firstword = 'Дев\'ятнадцять'
-            break;
-
-    }
-
-}
-
-function from20to99() {
-    switch (arrayofStrings[0]) {
-        case '2':
-            firstword = 'Двадцять';
-            break;
-        case '3':
-            firstword = 'Тридцять'
-            break;
-        case '4':
-            firstword = 'Сорок'
-            break;
-        case '5':
-            firstword = 'П\'ятдесят'
-            break;
-        case '6':
-            firstword = 'Шістдесять'
-            break;
-        case '7':
-            firstword = 'Сімдесят'
-            break;
-        case '8':
-            firstword = 'Вісімдесят'
-            break;
-        case '9':
-            firstword = 'Дев\'яносто'
-            break;
-    }
-    switch (arrayofStrings[1]) {
-
-        case '0':
-            secondword = ''
-            break;
-        case '1':
-            secondword = 'один'
-            break;
-        case '2':
-            secondword = 'два'
-            break;
-        case '3':
-            secondword = 'три'
-            break;
-        case '4':
-            secondword = 'чотири'
-            break;
-        case '5':
-            secondword = 'п\'ять'
-            break;
-        case '6':
-            secondword = 'шість'
-            break;
-        case '7':
-            secondword = 'сім'
-            break;
-        case '8':
-            secondword = 'вісім'
-            break;
-        case '9':
-            secondword = 'дев\'ять'
-            break;
-    }
-
-
-}
-
-function from100to999without10to19() {
-    switch (arrayofStrings[0]) {
-        case '1':
-            firstword = 'Сто'
-            break;
-        case '2':
-            firstword = 'Двісті'
-            break;
-        case '3':
-            firstword = 'Триста'
-            break;
-        case '4':
-            firstword = 'Чотириста'
-            break;
-        case '5':
-            firstword = 'П\'ятсот'
-            break;
-        case '6':
-            firstword = 'Шістсот'
-            break;
-        case '7':
-            firstword = 'Сімсот'
-            break;
-        case '8':
-            firstword = 'Вісімсот'
-            break;
-        case '9':
-            firstword = 'Дев\'ятсот'
-            break;
-    }
-
-    switch (arrayofStrings[1]) {
-
-        case '2':
-            secondword = 'двадцять'
-            break;
-        case '3':
-            secondword = 'тридцять'
-            break;
-        case '4':
-            secondword = 'сорок'
-            break;
-        case '5':
-            secondword = 'п\'ятдесят'
-            break;
-        case '6':
-            secondword = 'шістдесять'
-            break;
-        case '7':
-            secondword = 'сімдесят'
-            break;
-        case '8':
-            secondword = 'вісімдесят'
-            break;
-        case '9':
-            secondword = 'дев\'яносто'
-            break;
-    }
-    switch (arrayofStrings[2]) {
-        case '0':
-            thirdword = ''
-            break;
-        case '1':
-            thirdword = 'один'
-            break;
-        case '2':
-            thirdword = 'два'
-            break;
-        case '3':
-            thirdword = 'три'
-            break;
-        case '4':
-            thirdword = 'чотири'
-            break;
-        case '5':
-            thirdword = 'п\'ять'
-            break;
-        case '6':
-            thirdword = 'щість'
-            break;
-        case '7':
-            thirdword = 'сім'
-            break;
-        case '8':
-            thirdword = 'вісім'
-            break;
-        case '9':
-            thirdword = 'дев\'ять'
-            break;
-    }
-}
-
-function from100to999with10to19() {
-    switch (arrayofStrings[0]) {
-        case '1':
-            firstword = 'Сто'
-            break;
-        case '2':
-            firstword = 'Двісті'
-            break;
-        case '3':
-            firstword = 'Триста'
-            break;
-        case '4':
-            firstword = 'Чотириста'
-            break;
-        case '5':
-            firstword = 'П\'ятсот'
-            break;
-        case '6':
-            firstword = 'Шістсот'
-            break;
-        case '7':
-            firstword = 'Сімсот'
-            break;
-        case '8':
-            firstword = 'Вісімсот'
-            break;
-        case '9':
-            firstword = 'Дев\'ятсот'
-            break;
-    }
-    switch (arrayofStrings[1]) {
-        case '0':
-            firstword = 'десять'
-            break;
-        case '1':
-            firstword = 'одинадцять'
-            break;
-        case '2':
-            firstword = 'дванадцять'
-            break;
-        case '3':
-            firstword = 'тринадцять'
-            break;
-        case '4':
-            firstword = 'чотирнадцять'
-            break;
-        case '5':
-            firstword = 'п\'ятнадцять'
-            break;
-        case '6':
-            firstword = 'шістнадцять'
-            break;
-        case '7':
-            firstword = 'сімнадцять'
-            break;
-        case '8':
-            firstword = 'вісімнадцять'
-            break;
-        case '9':
-            firstword = 'дев\'ятнадцять'
-            break;
-
-    }
-
-}
+result = str1 + str2;
+console.log(result);
